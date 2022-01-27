966ab8457719c69c220e99a19bd0f1ff49d435a4
Max Waring <max.waring@spotlightsportsgroup.com>
2022-01-27 20:35:05 +0000

    max forgot to add a commit message, oops!

---
+++ frontend/src/FrontendApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FrontendApp = () => {
+  return (
+    <h1>
+     Welcome to a very blank frontend
+    </h1>
+  )
+}
+
+export default FrontendApp
---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.tsx"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
+};
+
